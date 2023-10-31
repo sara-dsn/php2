771c5e709aaaf919d13fef859f9ac845ecdd76d1
sara-dsn <sabr.tdg@gmail.com>
2023-10-31 15:34:14 +0100

    h

--- capitale.php
+++ capitale.php
@@ -6,6 +6,169 @@
     <title>Capitales</title>
 </head>
 <body>
+   <?php
+
+$capitales = array(
+
+    "Bucarest" => "Roumanie",
+
+    "Bruxelles" => "Belgique",
+
+    "Oslo" => "Norvège",
+
+    "Ottawa" => "Canada",
+
+    "Paris" => "France",
+
+    "Port-au-Prince" => "Haïti",
+
+    "Port-d'Espagne" => "Trinité-et-Tobago",
+
+    "Prague" => "République tchèque",
+
+    "Rabat" => "Maroc",
+
+    "Riga" => "Lettonie",
+
+    "Rome" => "Italie",
+
+    "San José" => "Costa Rica",
+
+    "Santiago" => "Chili",
+
+    "Sofia" => "Bulgarie",
+
+    "Alger" => "Algérie",
+
+    "Amsterdam" => "Pays-Bas",
+
+    "Andorre-la-Vieille" => "Andorre",
+
+    "Asuncion" => "Paraguay",
+
+    "Athènes" => "Grèce",
+
+    "Bagdad" => "Irak",
+
+    "Bamako" => "Mali",
+
+    "Berlin" => "Allemagne",
+
+    "Bogota" => "Colombie",
+
+    "Brasilia" => "Brésil",
+
+    "Bratislava" => "Slovaquie",
+
+    "Varsovie" => "Pologne",
+
+    "Budapest" => "Hongrie",
+
+    "Le Caire" => "Egypte",
+
+    "Canberra" => "Australie",
+
+    "Caracas" => "Venezuela",
+
+    "Jakarta" => "Indonésie",
+
+    "Kiev" => "Ukraine",
+
+    "Kigali" => "Rwanda",
+
+    "Kingston" => "Jamaïque",
+
+    "Lima" => "Pérou",
+
+    "Londres" => "Royaume-Uni",
+
+    "Madrid" => "Espagne",
+
+    "Malé" => "Maldives",
+
+    "Mexico" => "Mexique",
+
+    "Minsk" => "Biélorussie",
+
+    "Moscou" => "Russie",
+
+    "Nairobi" => "Kenya",
+
+    "New Delhi" => "Inde",
+
+    "Stockholm" => "Suède",
+
+    "Téhéran" => "Iran",
+
+    "Tokyo" => "Japon",
+
+    "Tunis" => "Tunisie",
+
+    "Copenhague" => "Danemark",
+
+    "Dakar" => "Sénégal",
+
+    "Damas" => "Syrie",
+
+    "Dublin" => "Irlande",
+
+    "Erevan" => "Arménie",
+
+    "La Havane" => "Cuba",
+
+    "Helsinki" => "Finlande",
+
+    "Islamabad" => "Pakistan",
+
+    "Vienne" => "Autriche",
+
+    "Vilnius" => "Lituanie",
+
+    "Zagreb" => "Croatie"
+
+);
+echo "<table border='1'><section><tr>";
+ksort($capitales);
+foreach ($capitales as $capitale => $pays) {
+    echo "<td>$capitale </td>
+    <td> $pays</td></tr>";
+ 
+ 
+};
+$nb=count($capitales);
+
+    echo "<td>Nombre de pays </td><td> $nb </td></tr>";
+ 
+
+echo "</table></section>";
+echo "<br><br><br>";
+asort($capitales);
+foreach ($capitales as $capitale => $pays) {
+    echo "<p>$pays : $capitale </p>";
+ 
+ 
+};
+
+    foreach($capitales as $capitale => $pays){
+       if( substr($capitale,0,1)!=="B"){
+        unset($capitales[$capitale]);
+       };
+    };
+    echo "<table border='1'><section><tr>";
+   
+    foreach ($capitales as $capitale => $pays) {
+        echo "<td>$capitale </td>
+        <td> $pays</td></tr>";
+     
+     
+    };
+
+     
+echo "</table></section>";
+echo "<br><br><br>";
     
+
+
+   ?> 
 </body>
 </html>
