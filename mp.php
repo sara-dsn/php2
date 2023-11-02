2960bf0eacb3424ddadf2e3119f0d219319532c1
sara-dsn <sabr.tdg@gmail.com>
2023-11-02 10:01:50 +0100

    5

--- mp.php
+++ mp.php
@@ -6,8 +6,32 @@
     <title>mp</title>
 </head>
 <body>
+    <form method="POST" action="">
+    Mot de passe: <input type="text" name="mp">
+    <button type="submit" name="btn">Envoyer</button>
+</form>
+  
     <?php
+
+if (isset($_POST["btn"])) {
+    $mp = $_POST["mp"];
+    function verif($mp) {
+        $valid = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/";
+
+        if (preg_match($valid, $mp)) {
+            return true;
+            echo "Mot de passe valide";
+        } else {
+            echo "Mot de passe invalide";
+            return false;
+        };
+    };
+
+    verif($mp);
     
+};
     ?>
+
+
 </body>
 </html>
